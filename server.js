7c84132a3b7c5c1e52076c72752509e74645489a
Rawan199812 <rawan.13579@icloud.com>
2021-03-31 22:47:35 +0300

    not finished

--- server.js
+++ server.js
@@ -8,39 +8,69 @@ const superagent = require('superagent')
 const app = express(); 
 const PORT = process.env.PORT || 3000;
 const PARKS_API_KEY = process.env.PARKS_API_KEY;
+const DATABASE_URL = process.env.DATABASE_URL;
+const NODE_ENV = process.env.NODE_ENV;
 
 app.use(cors());
-let locations={};
+const options = NODE_ENV === 'production' ? { connectionString: DATABASE_URL, ssl: { rejectUnauthorized: false } } : { connectionString: DATABASE_URL };
 
-const handelLocation = (request,response)=>{
-  // const location =require('./data/location.json');
-  const city = request.query.city;
+const pg=require('pg');
+const client=new pg.Client(options);
+
+// client.connect()
+//     .then(() => {
+//         // This will only start out webserver if we connected successfully
+//         app.listen(PORT, () => {
+//             console.log(`App listening on port ${PORT}`);
+//         })
+//     }).catch(error => {
+//         console.log('Unable to connect to database: ', error.message);
+//     });
+
+
+function handelLocation (request,response){
+  // const city = request.query.city;
+
   let key = process.env.GEOCODE_API_KEY;
+
+  const city = request.query.q;
+
   const url = `https://us1.locationiq.com/v1/search.php?key=${key}&q=${city}&format=json&limit=1`;
+  const select = 'SELECT * FROM locations WHERE search_query = $1';
 
-  if (locations[url]) {
-   
-    response.send(locations[url]);
-  } else {
-    superagent.get(url)
-    .then(data => {
-    
-    // https://us1.locationiq.com/v1/search.php?key=pk.97eb6319cc04540bb55f8dc4a7874b7e&q=Amman&format=json
-    const location = data.body[0];
-    const locationData  = new Locations (city,location);
-    locations[url] = locationData;
-    // response.send(locationData);
-    response.json(locationData );
+  client.query(select, [city])
+    .then(results => {
+      if (results.rowCount === 0) {
+        superagent.get(url)
+          .then(data => {
+            const geoData = data.body[0];
+            const locationInfo = new Locations(city, geoData);
+            const { search_query, formatted_query, latitude, longitude } = locationInfo;
+            let safeValues = [search_query, formatted_query, latitude, longitude];
+            let sql = 'INSERT INTO locations (search_query, formatted_query,latitude,longitude) VALUES ($1, $2,$3,$4) RETURNING *';
+            client.query(sql, safeValues)
+              .then((data) => {
+                response.json(data.rows[0]);
+              });
+          })
 
-}) 
-.catch((error) => errorHandler(error, request, response));  
-}     
-    } 
-    function errorHandler(error, request, response) {
-      response.status(500).send(error);
-  }
-  // changes
-    
+          .catch((error) => {
+            console.log('error from location', error);
+            response.status(500).send('So sorry, something went wrong.');
+          });
+
+      } else {
+        response.json(results.rows[0]);
+      }
+
+    }).catch(e=>console.log(e.message,'rejected'));
+
+};
+
+
+
+
+  //end  
   app.get('/parks', handelPark);
   function handelPark(request, response) {
     const url = `https://developer.nps.gov/api/v1/parks?parkCode=acad&api_key=${PARKS_API_KEY}`;
@@ -62,11 +92,6 @@ const handelLocation = (request,response)=>{
     const url=`https://api.weatherbit.io/v2.0/forecast/daily?city=Raleigh,NC&key=${key}`
     const rawWeatherData = await superagent.get(url);
     const weatherData = JSON.parse(rawWeatherData.text).data;
-
-    // let weatherArr = weather.data;
-    // weatherArr.forEach(element => {
-      //   let newW = new Weather(element.weather.description,element.valid_date);
-      //   weatherData.push(newW)
       
       // use map 
       const forecasts= weatherData.map(element => {
@@ -118,12 +143,21 @@ const handelLocation = (request,response)=>{
     console.log(request.query);
     response.send('work');
   };
+  client.connect()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log("Connected to database:", client.connectionParameters.database);
+      console.log('Server up on', PORT);
+    });
+  })
+  .catch(err => {
+    console.log('ERROR', err);
+  });
   
-  
-  
+
 app.get('/location',handelLocation);
 app.get('/weather',handelWeather);
 app.get('/', handleRequest);
 app.use('*', handelError);
-app.listen(PORT, () => console.log('app listening on port 3000!'));
+// app.listen(PORT, () => console.log('app listening on port 3000!'));
 
