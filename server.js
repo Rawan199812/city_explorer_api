65dc4d75e515996c65c4b5e2112c43a4d1875869
Rawan199812 <rawan.13579@icloud.com>
2021-04-03 10:05:32 +0300

    fix lab 8

--- server.js
+++ server.js
@@ -5,44 +5,49 @@ const cors = require('cors');
 const superagent = require('superagent')
 
 
-const app = express(); 
+const app = express();
 const PORT = process.env.PORT || 3000;
 const PARKS_API_KEY = process.env.PARKS_API_KEY;
 const DATABASE_URL = process.env.DATABASE_URL;
 const NODE_ENV = process.env.NODE_ENV;
-const MOVIE_API_KEY =process.env.MOVIE_API_KEY;
+const MOVIE_API_KEY = process.env.MOVIE_API_KEY;
 
 app.use(cors());
 const options = NODE_ENV === 'production' ? { connectionString: DATABASE_URL, ssl: { rejectUnauthorized: false } } : { connectionString: DATABASE_URL };
 
-const pg=require('pg');
-const client=new pg.Client(options);
+const pg = require('pg');
+const client = new pg.Client(options);
 
-function handelLocation (request,response){
-  // const city = request.query.city;
+function handelLocation(request, response) {
 
   let key = process.env.GEOCODE_API_KEY;
 
   const city = request.query.city;
+  const searchQuery = request.query.city;
+  const url = `https://us1.locationiq.com/v1/search.php?key=${key}&q=${searchQuery}&format=json&limit=1`;
+  const sql = 'SELECT * FROM locations WHERE search_query = $1';
+  let cityArr = [searchQuery];
 
-  const url = `https://us1.locationiq.com/v1/search.php?key=${key}&q=${city}&format=json&limit=1`;
-  const select = 'SELECT * FROM locations WHERE search_query = $1';
-
-  client.query(select, [city])
+  client.query(sql, cityArr)
     .then(results => {
-      console.log(city);
-      if (results.rowCount === 0) {
+
+      if (results.rowCount) {
 
         superagent.get(url)
           .then(data => {
-            const geoData = data.body[0];
-            const locationInfo = new Locations(city, geoData);
-            const { search_query, formatted_query, latitude, longitude } = locationInfo;
-            let safeValues = [search_query, formatted_query, latitude, longitude];
+            const formattedArr = data.body.map((loc) => {
+              return {
+                search_query: searchQuery,
+                formatted_query: loc.display_name,
+                latitude: loc.lat,
+                longitude: loc.lon
+              };
+            });
+            let values = [searchQuery, formattedArr[0].formatted_query, formattedArr[0].latitude, formattedArr[0].longitude];
             let sql = 'INSERT INTO locations (search_query, formatted_query,latitude,longitude) VALUES ($1, $2,$3,$4) RETURNING *';
-            client.query(sql, safeValues)
-              .then((data) => {
-                response.json(data.rows[0]);
+            client.query(sql, values)
+              .then((results) => {
+                response.json(formattedArr[0]);
               });
           })
 
@@ -55,107 +60,107 @@ function handelLocation (request,response){
         response.json(results.rows[0]);
       }
 
-    }).catch(e=>console.log(e.message,'rejected'));
+    }).catch(e => console.log(e.message, 'rejected'));
 
 };
-  app.get('/parks', handelPark);
-  function handelPark(request, response) {
-    const url = `https://developer.nps.gov/api/v1/parks?parkCode=acad&api_key=${PARKS_API_KEY}`;
-    superagent.get(url).then(parksData => {
-        const parks = parksData.body.data.map(data => {
+app.get('/parks', handelPark);
+function handelPark(request, response) {
+  const url = `https://developer.nps.gov/api/v1/parks?parkCode=acad&api_key=${PARKS_API_KEY}`;
+  superagent.get(url).then(parksData => {
+    const parks = parksData.body.data.map(data => {
 
-            return new Parks(data.fullName, data.addresses[0].line1 + data.addresses[0].city, data.entranceFees[0].cost, data.description, data.url)
-        });
-        response.status(200).json(parks);
-    }).catch((error) => {
-        console.error(error);
-        response.status(500).send('Something went wrong');
-    })
+      return new Parks(data.fullName, data.addresses[0].line1 + data.addresses[0].city, data.entranceFees[0].cost, data.description, data.url)
+    });
+    response.status(200).json(parks);
+  }).catch((error) => {
+    console.error(error);
+    response.status(500).send('Something went wrong');
+  })
 }
-  // let weatherData = [];
-  async function handelWeather(request,response){
-    try{
+// let weatherData = [];
+async function handelWeather(request, response) {
+  try {
     let key = process.env.WEATHER_API_KEY;
-    const url=`https://api.weatherbit.io/v2.0/forecast/daily?city=Raleigh,NC&key=${key}`
+    const url = `https://api.weatherbit.io/v2.0/forecast/daily?city=Raleigh,NC&key=${key}`
     const rawWeatherData = await superagent.get(url);
     const weatherData = JSON.parse(rawWeatherData.text).data;
-      
-      // use map 
-      const forecasts= weatherData.map(element => {
-        const description = element.weather.description;
-        const time = element.datetime;
-        return new Weather(description,time);
-        
-      })  
-      response.json(forecasts );
-    }catch(error) {
-      console.error(error);
-      response.status(500).send('Something went wrong');
+
+    // use map 
+    const forecasts = weatherData.map(element => {
+      const description = element.weather.description;
+      const time = element.datetime;
+      return new Weather(description, time);
+
+    })
+    response.json(forecasts);
+  } catch (error) {
+    console.error(error);
+    response.status(500).send('Something went wrong');
   }
-    
-    }  
-    const handelError = (request,response)=>{
-      response.status(500).send('Sorry, something went wrong');
-    };  
-    
-  function handelMovie(request,response) {
-    const url=`https://api.themoviedb.org/3/movie/550?api_key=${MOVIE_API_KEY}`
-    superagent.get(url)
+
+}
+const handelError = (request, response) => {
+  response.status(500).send('Sorry, something went wrong');
+};
+
+function handelMovie(request, response) {
+  const url = `https://api.themoviedb.org/3/movie/550?api_key=${MOVIE_API_KEY}`
+  superagent.get(url)
     .then(data => {
       console.log(data.results);
-        const movies = data.results.Data.map(Data => {
-            return new Movie(Data.title,Data.average_votes,Data.total_votes,Data.image_url,Data.popularity,Data.released_on)
-        })
-        response.status(200).send(movies)
+      const movies = data.results.Data.map(Data => {
+        return new Movie(Data.title, Data.average_votes, Data.total_votes, Data.image_url, Data.popularity, Data.released_on)
+      })
+      response.status(200).send(movies)
     })
-    .catch(e=>console.log(e.message,'rejected'));    
+    .catch(e => console.log(e.message, 'rejected'));
 
 
-    
-  }  
 
-  
-  //Location  Constructor 
-  function Locations(city,location) {
-    this.search_query = city;
-    this.formatted_query =location.display_name;
-    this.latitude = location.lat;
-    this.longitude = location.lon;
-  }
-  
-  
-  
-  // Weather Constructor
-  function Weather(description,valid_date) {
-    this.forecast=description;
-    this.time=valid_date; //.toDateString()
-    
-  }
-  // Park Constructor
-  function Parks(name, address, fee, description, url) {
-    this.name = name;
-    this.address = address;
-    this.fee = fee;
-    this.description = description;
-    this.url = url;
+}
+
+
+//Location  Constructor 
+function Locations(city, location) {
+  this.search_query = city;
+  this.formatted_query = location.display_name;
+  this.latitude = location.lat;
+  this.longitude = location.lon;
+}
+
+
+
+// Weather Constructor
+function Weather(description, valid_date) {
+  this.forecast = description;
+  this.time = valid_date; //.toDateString()
+
+}
+// Park Constructor
+function Parks(name, address, fee, description, url) {
+  this.name = name;
+  this.address = address;
+  this.fee = fee;
+  this.description = description;
+  this.url = url;
 }
 // Movie Constructor
 function Movie(Data) {
-  this.title=title;
-  this.overview=overview;
-  this.average_votes=average_votes;
-  this.total_votes=total_votes;
-  this.image_url=image_url;
-  this.popularity=popularity;
-  this.released_on=released_on;
-  
+  this.title = title;
+  this.overview = overview;
+  this.average_votes = average_votes;
+  this.total_votes = total_votes;
+  this.image_url = image_url;
+  this.popularity = popularity;
+  this.released_on = released_on;
+
 }
-  
-  const handleRequest = (request, response) => {
-    console.log(request.query);
-    response.send('work');
-  };
-  client.connect()
+
+const handleRequest = (request, response) => {
+  console.log(request.query);
+  response.send('work');
+};
+client.connect()
   .then(() => {
     app.listen(PORT, () => {
       console.log("Connected to database:", client.connectionParameters.database);
@@ -165,12 +170,12 @@ function Movie(Data) {
   .catch(err => {
     console.log('ERROR', err);
   });
-  
 
-app.get('/location',handelLocation);
-app.get('/weather',handelWeather);
-app.get('/movie',handelMovie)
+
+app.get('/location', handelLocation);
+app.get('/weather', handelWeather);
+app.get('/movie', handelMovie)
 app.get('/', handleRequest);
 app.use('*', handelError);
-// app.listen(PORT, () => console.log('app listening on port 3000!'));
+
 
